3c8b105864b8cb1a37c76e2eb77518ec6fedf68d
Jan Prasil <prasil.honza@gmail.com>
2016-12-15 18:20:28 +0100

    DTW endpoint - read input parameters from Firebase database

--- src/vmm.api/Controllers/DtwController.cs
+++ src/vmm.api/Controllers/DtwController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Firebase.Database;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
 using vmm.api.Data;
+using vmm.api.Models;
 using vmm.api.Services;
 
 namespace vmm.api.Controllers
@@ -9,20 +14,34 @@ namespace vmm.api.Controllers
     public class DtwController : Controller
     {
         private IContoursManager contoursManager;
+        private IDbManager dbManager;
 
-        public DtwController(IContoursManager contoursManager)
+        public DtwController(IContoursManager contoursManager, IDbManager dbManager)
         {
             this.contoursManager = contoursManager;
+            this.dbManager = dbManager;
         }
 
-        [HttpPost]
-        public JsonResult runDtw(string refId)
+        [HttpGet]
+        public async Task<JsonResult> runDtw(string sessionId, string referenceId)
         {
-            if (ShapeStorage.list.Count >= 2)
+
+            var result = new List<FirebaseObject<Result>>();
+            var images = await dbManager.GetAllAsync<Shape>(new string[] { "Images", sessionId });
+            var reference = await dbManager.GetAsync<Shape>(new string[] { "ReferenceSamples", referenceId });
+
+            if (images.Count == 0) return Json(new { id = "NO_IMAGES_SELECTED" });
+            if (reference == null) return Json(new { id = "NO_REFERENCE_SELECTED" });
+            foreach (var x in images)
             {
-                return Json(contoursManager.DynamicTimeWarping(ShapeStorage.list[0], ShapeStorage.list[1]));
+                var r = new Result() {
+                    result = contoursManager.DynamicTimeWarping(reference, x.Object),
+                    imageId = x.Key,
+                    referenceId = referenceId
+                };
+                result.Add(await dbManager.PostAsync(new string[] { "Results" }, r));
             }
-            return Json(new { Id = "TWO_IMAGES_NEEDED", Message = "Upload at least two images" });
+            return Json(result);
         }
     }
 }
---
+++ src/vmm.api/Models/Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vmm.api.Models
+{
+    public class Result : IModel    
+    {
+        public string referenceId { get; set; }
+        public string imageId { get; set; }
+        public IEnumerable<Point> result { get; set; }
+    }
+}
